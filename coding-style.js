0717407eabee42c32456c64b43cbf01469892a01
Ziad Saab <ziad.saab@gmail.com>
2016-07-04 20:23:50 +0000

    Added curly bracket section to coding style guide

--- coding-style.js
+++ coding-style.js
@@ -1,5 +1,24 @@
+// Variable assignment
 var age = 31; // <- semicolon at the end of each statement
 //     ^ ^ having white space around = sign is good :)
 // it makes the code more readable
 
 var name="Ziad";
+
+
+// Curly brackets
+var num = -10;
+
+if (num > 0) { // <- always have {} even if you have one statement. if/else/for/while
+    console.log('the number is greater than zero');
+    
+}
+
+if (num > 0) {
+    // .. code goes here
+}
+
+if (num > 0)
+{
+    // .. code goes here
+}
